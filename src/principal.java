93bdb87d18143719aed3341bc016380ad54446f3
oscar <oscargoru04@gmail.com>
2023-03-17 11:09:29 +0100

    implementación de clase suma

---
+++ src/principal.java
@@ -0,0 +1,18 @@
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int numero1=1;
+		int numero2=3;
+		
+	numero(numero1,numero2);
+
+	}
+		public static void numero (int numero1,int numero2) {
+			int suma =numero1+numero2;
+			System.out.println("La suma de los números "+numero1+" y "+numero2+" es "+suma);
+		}
+}
